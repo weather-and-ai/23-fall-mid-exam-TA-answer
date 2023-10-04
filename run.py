28f6a77c17a44591c6344834572e01f95cd5ef56
1chooo <hugo970217@gmail.com>
2023-10-04 14:12:29 +0800

    add: login ee-class

--- run.py
+++ run.py
@@ -5,10 +5,22 @@ Author: @1chooo (Hugo ChunHo Lin)
 Version: v0.0.1
 '''
 
-import requests
-from bs4 import BeautifulSoup
-import json
-import config
+from EEJudge.Login import Bot
+import aiohttp
+import asyncio
+from dotenv import dotenv_values
 
-BASE_URL = "https://ncueeclass.ncu.edu.tw"
-LOGIN_URL = BASE_URL + "/index/login"
+config = dotenv_values(".env")
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+
+        bot = Bot(
+            session=session,
+            account=config["account"],
+            password=config["password"]
+        )
+        await bot.login()
+
+if __name__ == '__main__':
+    asyncio.run(main())
